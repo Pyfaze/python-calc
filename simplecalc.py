a88de21ed97347da1d577b1345c2aae2f8b18604
Nick <127917129+Pyfaze@users.noreply.github.com>
2023-03-15 09:16:19 -0400

    Python Calculator
    Simple command line calculator

---
+++ simplecalc.py
@@ -0,0 +1,41 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def main():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter the number of the operation you want to perform (1/2/3/4): ")
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        print(f"{num1} + {num2} = {add(num1, num2)}")
+    elif choice == '2':
+        print(f"{num1} - {num2} = {subtract(num1, num2)}")
+    elif choice == '3':
+        print(f"{num1} * {num2} = {multiply(num1, num2)}")
+    elif choice == '4':
+        if num2 != 0:
+            print(f"{num1} / {num2} = {divide(num1, num2)}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Invalid input. Please try again.")
+
+if __name__ == "__main__":
+    main()
+
